590cffd5ba59699c30d91bd9ee0f44b6d88bcbe6
root <kaliq.mcgee@mavenwave.comgit config --global user.name Kaliq>
2022-04-13 21:04:20 +0000

    Updated vpc module again and again and again

--- vpc.tf
+++ vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
     version = "~> 3.0"
 
     project_id   = var.project
-    network_name = kmsai-network
+    network_name = "kmsai-network"
     routing_mode = "GLOBAL"
   
   subnets = [
